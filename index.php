28a5c5cee524dcc954f266b729ab50b98fa8bc21
Ivan Doric <ivan.doric@gauss.hr>
2016-03-22 18:06:24 +0100

    Theming with WP REST API - Part 02 - Listing Posts

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php get_header(); ?>
 
+    <div class="white-wrap">
+        <div id="app">
+
+            <router-view></router-view>
+
+        </div>
+    </div>
+
+
+<template id="post-list-template">
+    <div class="container">
+        <div class="post-list">
+            <article v-for="post in posts" class="post">
+                <div class="post-content">
+                    <h2>{{ post.title.rendered }}</h2>
+                </div>
+            </article>
+        </div>
+    </div>
+
+</template>
 
 <?php get_footer(); ?>
---
+++ js/app.js
@@ -0,0 +1,30 @@
+var App = Vue.extend({});
+
+var postList = Vue.extend({
+    template:'#post-list-template',
+    data: function(){
+        return {
+            posts: ''
+        }
+    },
+
+    ready: function(){
+        posts = this.$resource('/wp-json/wp/v2/posts?per_page=20');
+
+        posts.get(function(posts){
+            this.$set('posts', posts);
+        })
+    }
+})
+
+
+
+var router = new VueRouter();
+
+router.map({
+    '/':{
+        component: postList
+    }
+});
+
+router.start(App, '#app');
